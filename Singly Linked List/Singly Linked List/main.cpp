3603698da9acb2d45da028fd7111bbf609929969
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-07-14 22:31:32 +0530

    Singly Linked List Complete Class

---
+++ Singly Linked List/Singly Linked List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Singly Linked List
+//
+//  Created by Hemant Jadon on 14/07/16.
+//  Copyright (c) 2016 Hemant Jadon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
