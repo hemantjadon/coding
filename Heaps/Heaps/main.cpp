c36eb6e513591897a9d7cd4d5701aecd7e849752
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-07-26 18:41:10 +0530

    Heaps

--- Heaps/Heaps/main.cpp
+++ Heaps/Heaps/main.cpp
@@ -24,6 +24,14 @@ namespace Heap {
         Heap(bool heap_type=false):heap_type(heap_type),size(0)
         {}
         
+        bool isMinHeap(){
+            return !heap_type;
+        }
+        
+        vector<int> getHeapVector(){
+            return this->heap;
+        }
+        
         void formHeap(int arr[],int n){
             for (int i=0; i<n; i++) {
                 this->insert(arr[i]);
@@ -38,12 +46,53 @@ namespace Heap {
             }
         }
         
-        long getSize() const{
-            return this->size;
+        void rebuild(int i=0){
+            int l = 2*i+1;
+            int r = 2*i+2;
+            
+            if (heap_type == false) {
+                //  Min Heap
+                int smallest = i;
+                
+                if (l < heap.size() && heap[l] < heap[smallest]) {
+                    smallest = l;
+                }
+                
+                if (r < heap.size() && heap[r] < heap[smallest]) {
+                    smallest = r;
+                }
+                
+                if (smallest != i) {
+                    swap(heap[i], heap[smallest]);
+                    rebuild(smallest);
+                }
+            }
+            else {
+                //  Max Heap
+                int largest = i;
+                
+                if (l < heap.size() && heap[l] > heap[largest]) {
+                    largest = l;
+                }
+                
+                if (r < heap.size() && heap[r] > heap[largest]) {
+                    largest = r;
+                }
+                
+                if (largest != i) {
+                    swap(heap[i], heap[largest]);
+                    rebuild(largest);
+                }
+            }
         }
         
-        bool isMinHeap(){
-            return !heap_type;
+        void changeValue(int i,int new_val){
+            heap[i]=new_val;
+            this->rebuild(i);
+        }
+        
+        long getSize() const{
+            return this->size;
         }
         
         void printHeap(){
@@ -147,7 +196,7 @@ namespace Heap {
             vector<int>::iterator it;
             
             for (it=heap.begin()+final_idx; it<heap.end(); it++) {
-                __swap((*it), (*(it+1)));
+                swap((*it), (*(it+1)));
             }
             
             heap.erase(it-1);
@@ -155,57 +204,183 @@ namespace Heap {
             this->size--;
             return most_prior;
         }
-        
-    private:
-        // Auxilary Functions
-        void __swap(int &a, int&b){
-            int temp = a;
-            a = b;
-            b = temp;
-        }
     };
     
     void heapify(int arr[],int i,int n,bool heap_type=false){
-        int largest = i;
-        int l = 2*i + 1;
-        int r = 2*i + 2;
+        int l = 2*i+1;
+        int r = 2*i+2;
         
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-        
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-        
-        if (largest != i)
-        {
-            swap(arr[i], arr[largest]);
+        if (heap_type == false) {
+            //  Min Heap
+            int smallest = i;
             
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
+            if (l < n && arr[l] < arr[smallest]) {
+                smallest = l;
+            }
+            
+            if (r < n && arr[r] < arr[smallest]) {
+                smallest = r;
+            }
+            
+            if (smallest != i) {
+                swap(arr[i], arr[smallest]);
+                heapify(arr, smallest, n, heap_type);
+            }
+        }
+        else {
+            //  Max Heap
+            int largest = i;
+            
+            if (l < n && arr[l] > arr[largest]) {
+                largest = l;
+            }
+            
+            if (r < n && arr[r] > arr[largest]) {
+                largest = r;
+            }
+            
+            if (largest != i) {
+                swap(arr[i], arr[largest]);
+                heapify(arr, largest, n, heap_type);
+            }
         }
     }
     
     void heapSort(int arr[],int n){
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+        //  Starting the heaping process with parent of last element.
+        for (int i=n/2-1; i>=0; i--) {
+            heapify(arr, i, n, true);
+        }
         
-        for (int i=n-1; i>=0; i--)
-        {
+        for (int i=n-1; i>=0; i--) {
             swap(arr[0], arr[i]);
-            
-            heapify(arr, i, 0);
+            heapify(arr, 0, i-1, true);
         }
     }
+    
+    vector<int> getKLargest(int arr[],int n,int k){
+        /**
+         *  The time complexiety of this function is O(n+klog(k))
+         *  The time complexiety for building heap O(n)
+         *  Then extracting k elements from it O(klog(k))
+         */
+        
+        vector<int> vct;
+        Heap h(true);
+        h.formHeap(arr, n);
+        for (int i=0; i<k; i++) {
+            vct.push_back(h.extractMostPrior());
+        }
+        return vct;
+    }
+    
+    vector<int> getKLargest_using_MinHeap(int arr[],int n,int k,bool order_sorted=true){
+        /**
+         *  The time complexiety of this function if sorted is required => O(k+n*log(k))
+         *  The time complexiety for building heap O(k)
+         *  Then changing the most prior atmost (n-k) times O((n-k)*log(k))
+         *  Then extracting the k elements from heap one-by-onr O(k*log(k))
+         *  So in total O(k + (n-k)*log(k) + k*log(k))
+         *
+         *  If Sorting is not required then => O(k+(n-k)*log(k))
+         *  The time complexiety for building heap O(k)
+         *  Then changing the most prior atmost (n-k) times O((n-k)*log(k))
+         *  Then iterating over the heap to get the most prior elements O(k)
+         *  So in total O(k + (n-k)*log(k))
+         */
+        
+        vector<int> vct;
+        
+        Heap h;
+        h.formHeap(arr, k);
+        h.printHeap();
+        for (int i=k; i<n; i++) {
+            if (arr[i] > h.getMostPrior()) {
+                h.changeValue(0, arr[i]);
+            }
+        }
+        
+        if (order_sorted) {
+            while (h.getSize()) {
+                vct.push_back(h.extractMostPrior());
+            }
+        }
+        
+        else {
+            vector<int> heap_vct = h.getHeapVector();
+            vector<int>::iterator it;
+            for (it=heap_vct.begin(); it<heap_vct.end(); it++) {
+                vct.push_back((*it));
+            }
+        }
+        
+        return vct;
+    }
+    
+    void sort_almost_sorted(int arr[],int n,int k){
+        Heap h;
+        h.formHeap(arr, k);
+        for (int i=k,j=0; i<n ; i++,j++) {
+            arr[j] = h.extractMostPrior();
+            h.insert(arr[i]);
+        }
+        int i=k;
+        while (h.getSize()) {
+            arr[i]=h.extractMostPrior();
+            i++;
+        }
+    }
+    
+    bool isArrayMinHeap(int arr[],int n){
+        bool is_heap = true;
+        
+        for (int i=0; i<n/2-1; i++) {
+            int l = 2*i+1;
+            int r = 2*i+2;
+            
+            if (l < n && arr[i] > arr[l]) {
+                is_heap = false;
+                break;
+            }
+            
+            if (r < n && arr[i] > arr[r]) {
+                is_heap = false;
+                break;
+            }
+        }
+        
+        return is_heap;
+    }
+    
+    bool isArrayMaxHeap(int arr[],int n){
+        bool is_heap = true;
+        
+        for (int i=0; i<n/2-1; i++) {
+            int l = 2*i+1;
+            int r = 2*i+2;
+            
+            if (l < n && arr[i] < arr[l]) {
+                is_heap = false;
+                break;
+            }
+            
+            if (r < n && arr[i] < arr[r]) {
+                is_heap = false;
+                break;
+            }
+        }
+        
+        return is_heap;
+    }
+
+    
 }
 
 
 
 int main(int argc, const char * argv[]) {
     Heap::Heap h;
-    int arr[] = {1,3,2,4};
+    int arr[] = {9, 15, 10, 7, 12, 11};
     int n = sizeof(arr)/sizeof(int);
-    Heap::heapSort(arr, n);
-    for (int i=0; i<n; i++) {
-        cout << arr[i] << " ";
-    }
+    cout << Heap::isArrayMaxHeap(arr, n) << endl;
 }
