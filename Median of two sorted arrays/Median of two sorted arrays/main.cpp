480c9be2be830e6203584374d93d4ce4635b4a7c
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-07-26 23:33:06 +0530

    Median of two arrays - version 2

--- Median of two sorted arrays/Median of two sorted arrays/main.cpp
+++ Median of two sorted arrays/Median of two sorted arrays/main.cpp
@@ -39,7 +39,7 @@ int median_of_two_sorted_arrays_equal(int arr1[],int arr2[],int n){
 
 int median_of_two_sorted_arrays_equal_better(int arr1[],int arr2[],int low1,int high1,int low2, int high2){
     if (high1-low1 == 1) {
-        return min(max(arr1[low1], arr2[low2]),min(arr1[high1], arr2[high2]));
+        return (max(arr1[low1], arr2[low2])+min(arr1[high1], arr2[high2]))/2;
     }
     
     int m1_idx = low1 + (high1-low1)/2;
@@ -57,13 +57,172 @@ int median_of_two_sorted_arrays_equal_better(int arr1[],int arr2[],int low1,int 
     }
 }
 
+int median_of_two_sorted_arrays_different(int A[],int B[],int low1,int high1,int low2,int high2){
+    
+    int medianA_idx = low1 + (high1-low1)/2;
+    int medianB_idx = low2 + (high2-low2)/2;
+    
+    int medianA = A[medianA_idx];
+    int medianB = B[medianB_idx];
+    
+    //  Both arrays have 0 size.
+    if (low1 > high1 && low2 > high2) {
+        return -1;
+    }
+    
+    //  First array has 0 size
+    if (low1 > high1) {
+        return medianB;
+    }
+    
+    //  Second array has 0 size
+    if (low2 > high2) {
+        return medianA;
+    }
+    
+    //  Both arrays have 1 size.
+    if (high1-low1 == 0 && high2-low2 == 0) {
+        return max(medianA,medianB);
+    }
+    
+    //  First array of 1 size
+    if (high1-low1 == 0) {
+        if (A[0] == medianB) {
+            return medianB;
+        }
+        else if(A[0] > medianB){
+            if ((high2-low2+1)%2) {
+                //  Even elements in B
+                return B[medianB_idx+1];
+            }
+            else {
+                //  Odd elements in B
+                return medianB;
+            }
+        }
+        else {
+            if ((high2-low2+1)%2) {
+                //  Even elements in B
+                return medianB;
+            }
+            else {
+                //  Odd elements in B
+                return B[medianB_idx-1];
+            }
+        }
+    }
+    
+    //  Second array of size 1.
+    if (high2-low2 == 0) {
+        if (B[0] == medianA) {
+            return medianA;
+        }
+        else if(B[0] > medianA){
+            if ((high2-low2+1)%2) {
+                //  Even elements in B
+                return B[medianA_idx+1];
+            }
+            else {
+                //  Odd elements in B
+                return medianA;
+            }
+        }
+        else {
+            if ((high1-low1+1)%2) {
+                //  Even elements in B
+                return medianA;
+            }
+            else {
+                //  Odd elements in B
+                return B[medianA_idx-1];
+            }
+        }
+    }
+    
+    // Both Arrays of size 2.
+    if (high1-low1 == 1 && high2-low2 == 1) {
+        return (max(A[low1],B[low2]) + min(A[high1],B[high2]))/2;
+    }
+    
+    //  First array has size 2.
+    if (high1-low1 == 1) {
+        if ((high2-low2+1)%2 == 0) {
+            //  Even elements in B
+            int medianB_idx1 = medianB_idx;
+            int medianB_idx2 = medianB_idx+1;
+            int medianB1 = medianB;
+            int medianB2 = B[medianB_idx1];
+            
+            if (A[low1] <= medianB1 && A[high1] >= medianB2) {
+                return (medianB1 + medianB2)/2;
+            }
+            else if (A[high1] < medianB1){
+                if (A[high1] > B[medianB_idx1-1]) {
+                    return (medianB1 + B[medianB_idx1-1])/2;
+                }
+                else {
+                    return (A[high1] + medianB1)/2;
+                }
+            }
+            else if (A[low1] > medianB2){
+                if (A[low1] > B[medianB_idx2+1]) {
+                    return (medianB2 + B[medianB_idx2+1])/2;
+                }
+                else {
+                    return (A[low1] + medianB2)/2;
+                }
+            }
+            else if (A[low1] == medianB1 && A[high1] == medianB2){
+                return (medianB1+medianB2)/2;
+            }
+            else if (A[high1] == medianB1){
+                return medianB1;
+            }
+            else if (A[low1] == medianB2){
+                return medianB2;
+            }
+        }
+        else {
+            //  Odd elements in B
+            if (A[high1] == medianB || A[low1] == medianB) {
+                return medianB;
+            }
+            else if (A[high1] < medianB) {
+                if (A[high1] < B[medianB_idx-1]) {
+                    return B[medianB_idx-1];
+                }
+                else {
+                    return A[high1];
+                }
+            }
+            else if(A[low1] > medianB){
+                if (A[low1] > B[medianB_idx+1]) {
+                    return B[medianB_idx+1];
+                }
+                else {
+                    return A[low1];
+                }
+            }
+            else {
+                return medianB;
+            }
+        }
+    }
+    
+    if (medianA > medianB) {
+        return median_of_two_sorted_arrays_different(A, B, low1, medianA_idx, medianB_idx, high2);
+    }
+    else {
+        return median_of_two_sorted_arrays_different(A, B, medianA_idx, high1, low2, medianB_idx);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    int arr1[] = {1,12,15,26,38};
-    int arr2[] = {2,13,17,30,35};
+    int arr1[] = {1,3};
+    int arr2[] = {2,4,6,8};
     
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
     int n2 = sizeof(arr2)/sizeof(arr2[0]);
     
-    cout << median_of_two_sorted_arrays_equal_better(arr1, arr2, 0, n1-1, 0, n2-1) << endl;
+    cout << median_of_two_sorted_arrays_different(arr1, arr2, 0, n1-1, 0, n2-1) << endl;
 }
