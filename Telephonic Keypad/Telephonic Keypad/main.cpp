c1ebe06fd10fc0586c2d11872435b7e12ecb7811
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-07-27 17:59:44 +0530

    Telephonic keypad

--- Telephonic Keypad/Telephonic Keypad/main.cpp
+++ Telephonic Keypad/Telephonic Keypad/main.cpp
@@ -10,22 +10,23 @@
 #include <vector>
 using namespace std;
 
-const string Mapping[10] = {"","","abc","def","ghi","jkl","m no","pqrs","tuv","wxyz"};
+const string Mapping[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-void telephonic_keypad(int number[],int n,int curr_digit=0, string output=""){
-    if (curr_digit == n) {
+void telephonic_keypad(int number[],int n,int curr_num=0,string output=""){
+    if (output.length() == n) {
         cout << output << endl;
         return;
     }
-    for (int i=0; i < Mapping[number[curr_digit]].length(); i++) {
-        output += Mapping[number[curr_digit]][i];
-        telephonic_keypad(number,n,curr_digit+1,output);
+    
+    for (int i=0; i<Mapping[number[curr_num]].length(); i++) {
+        output += Mapping[number[curr_num]][i];
+        telephonic_keypad(number, n, curr_num+1, output);
         output.pop_back();
     }
-    
 }
 
 int main(int argc, const char * argv[]) {
-    int number[] = {1,2};
-    telephonic_keypad(number,2);
+    int number[] = {7,8};
+    int n = sizeof(number)/sizeof(number[0]);
+    telephonic_keypad(number, n);
 }
